b553f115d282b11383bf447bcbf3776b88608082
vladimir.horky <vladimir.horky@tedom.com>
2026-04-23 13:18:15 +0200

    Update

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Core\\' => array($baseDir . '/src'),
+    'core-mini\\' => array($baseDir . '/src'),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'xvladah/core',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '62218eb197578574bf65c08f044cbed45bb65b30',
+        'name' => 'xvladah/core-mini',
+        'pretty_version' => '1.2.0',
+        'version' => '1.2.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'xvladah/core' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '62218eb197578574bf65c08f044cbed45bb65b30',
+        'xvladah/core-mini' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
